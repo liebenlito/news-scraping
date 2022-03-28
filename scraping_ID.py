8fbfdf815900854755bee41c634f1ab62c9a90b8
muhazaliebenlito@gmail.com <muhazaliebenlito@gmail.com>
2022-03-28 17:35:02 +0700

    adding detik scraping

--- scraping_ID.py
+++ scraping_ID.py
@@ -46,6 +46,46 @@ def get_articles_kompas(url=" "):
     else:
         return "Artikel berita tidak dapat diakses."
 
+def get_articles_detik(url=" "):
+    res = requests.get(url)
+    
+    if res.status_code == 200: # Cek apakah bisa diakses
+        soup = BeautifulSoup(res.text, 'html.parser')
+        
+        # Ambil metadata
+        title = soup.find("meta", property="og:title")
+        description = soup.find("meta", property="og:description")
+        url = soup.find("meta", property="og:url")
+        published_date = soup.find("meta", attrs={'name': 'pubdate'})
+
+        # Mencari class/div untuk isi berita
+        articles = soup.find(class_="detail__body-text")
+        contents = articles.find_all("p")
+        
+        items = list()
+        for item in contents:
+            items.append(item.text)
+
+        # Filter konten berita
+        r = re.compile(".*Baca juga:")
+        filter_items = list(filter(r.match, items))
+        filter_contents = list(set(items) - set(filter_items))
+
+        # Gabung elemen-elemen di list
+        filter_contents = " ".join(filter_contents)
+
+        # Simpan data dalam dictionary
+        data = dict()
+        data = {'title': title["content"],
+                'description': description["content"],
+                'url': url["content"],
+                'published_date': published_date["content"],
+                'content': filter_contents
+                }
+        return data
+    
+    else:
+        return "Artikel berita tidak dapat diakses."
 
 def save_dump_file(dataset = list(), name='unknown'):
     now = datetime.datetime.now()
